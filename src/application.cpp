25482a6fdc3fe09d17d944f427b7e9f19dab0eb5
Filip Peterek <fpeterek@seznam.cz>
2020-12-11 19:39:20 +0100

    Moved spotlight slightly

--- src/application.cpp
+++ src/application.cpp
@@ -301,7 +301,7 @@ Application::Application() {
         .emplaceLight(glm::vec3 { 1.f }, glm::vec3 { 0.f, 1.f, 0.f }, gl::Light::Point)
         .emplaceLight(glm::vec3 { 1.f }, glm::vec3 { 0.f, 0.f, 8.0f }, gl::Light::Point)
         .emplaceLight(glm::vec3 { 1.f }, glm::vec3 { -3.f, -8.f, 0.f }, gl::Light::Directional)
-        .emplaceLight(glm::vec3 { 1.f, 0.f, 0.f }, glm::vec3 { -5.f, 2.f, 0.f }, glm::vec3 { 0.f, -2.f, 0.f }, 12.5f)
+        .emplaceLight(glm::vec3 { 1.f, 0.f, 0.f }, glm::vec3 { -10.f, 1.f, -5.f }, glm::vec3 { 0.f, -2.f, 0.f }, 12.5f)
         // .emplaceLight(glm::vec3 { 1.f }, glm::vec3 { 0.f, 6.f, 6.0f })
         // .emplaceLight(glm::vec3 { 1.f }, glm::vec3 { 0.f, -6.f, -6.0f })
         // .emplaceLight(glm::vec3 { 1.f }, glm::vec3 { 0.f, 0.f, -8.0f })
